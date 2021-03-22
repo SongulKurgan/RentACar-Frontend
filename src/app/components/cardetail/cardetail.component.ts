1659d0ca50a9d5f441e46541b77c8b1054264c56
SongulKurgan <songulkurgan36@gmail.com>
2021-03-22 12:20:01 +0300

    18.Hafta

--- src/app/components/cardetail/cardetail.component.ts
+++ src/app/components/cardetail/cardetail.component.ts
@@ -2,8 +2,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Cardetail } from 'src/app/models/cardetails';
+import { CarImage } from 'src/app/models/carimage';
 
 import { CardetailService } from 'src/app/services/cardetail.service';
+import { CarimageService } from 'src/app/services/carimage.service';
 
 @Component({
   selector: 'app-cardetail',
@@ -12,22 +14,31 @@ import { CardetailService } from 'src/app/services/cardetail.service';
 })
 export class CardetailComponent implements OnInit {
   car: Cardetail 
+  carimages:CarImage[]
+
   dataLoaded = false;
   constructor(private cardetailService: CardetailService,
-    private activatedRoute:ActivatedRoute) {}
+    private activatedRoute:ActivatedRoute,
+    private carimageService:CarimageService) {}
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params=>{
       if(params["carId"]){
         this.getCardetailsByCarId(params["carId"]);
+        this.getCarImagesByCarId(params["carId"]);
       }
     })
   }
   getCardetailsByCarId(carId:number) {
     this.cardetailService.getCarDetailsByCarId(carId).subscribe((response) => {
       this.car = response.data[0];
-      console.log(response)
       this.dataLoaded = true;
     });
+  }
+  getCarImagesByCarId(carId:number){
+    this.carimageService.getCarImagesByCarId(carId).subscribe(response=>{
+      this.carimages=response.data;
+      console.log(response)
+    })
   }
 }
